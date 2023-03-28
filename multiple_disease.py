cd2639932eabba33e3ddcf2b35431852a7de25fa
Pooja <101863282+xdpooja@users.noreply.github.com>
2023-03-28 13:10:56 +0530

    Update multiple_disease.py

--- multiple_disease.py
+++ multiple_disease.py
@@ -9,6 +9,7 @@ import pickle
 import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
+from pathlib import Path
 here = Path(__file__).parent
 
 diabetes_model = pickle.load(open(here / 'diabetes_model.sav','rb'))
